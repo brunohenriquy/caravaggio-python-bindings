28f4feec7555cb62e7d331ed064ed7c28b5ef579
xalperte <javier.alperte@gmail.com>
2019-11-25 17:47:58 +0100

    The API controls Throttle exceptions. The request will be processes N times (n_tries) for every M seconds (sec_btw_tries) throttled requests transparently for the user.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -6,5 +6,46 @@ Changelog
 Current
 =======
 
+- Use of coreapi and coreapi-cli libraries to implement API-Client code for
+caravaggio projects.
+
 - Base classes to implement API bindings in python for Caravaggio RESTful APIs
 
+- Control of Throttling exceptions (HTTP 429 - Too many Requests), the client
+will try tenaciously to execute the request 12 times waiting 5 seconds between
+try.
+
+- The number of tries and seconds between requests can be overwritten
+by request
+
+.. code-block:: python
+   :linenos:
+
+    return self.action(
+        ['dealflow', 'backlog_search_list'],
+        params=params,
+        validate=False,
+        n_tries=5,
+        sec_btw_tries=15)
+
+
+- Ability to redefine the name of the environment variables per Client
+implementation. By default the name of these variables are:
+`CARAVAGGIO_TOKEN` and `CARAVAGGIO_DOMAIN`, but this can be overwritten by
+client. For instance, an Client API for an application named Apian, could
+define its own variables as `APIAN_TOKEN` and `APIAN_DOMAIN`.
+
+.. code-block:: python
+   :linenos:
+
+    from caravaggio_python_bindings.api import CaravaggioAPI
+
+    class ApianAPI(CaravaggioAPI):
+        """
+        ApianAPI represents a binding for the Apian RESTful app.
+        """
+
+        CARAVAGGIO_DOMAIN = "APIAN_DOMAIN"
+        CARAVAGGIO_TOKEN = "APIAN_TOKEN"
+
+        default_domain = "https://apian.io"
--- src/caravaggio_python_bindings/resource.py
+++ src/caravaggio_python_bindings/resource.py
@@ -3,12 +3,26 @@
 # All rights reserved.
 # This software is proprietary and confidential and may not under
 # any circumstances be used, copied, or distributed.
+import logging
+import time
+from coreapi import exceptions
+
+from caravaggio_python_bindings import status
+
+_logger = logging.getLogger("caravaggio_python_bindings.resource")
 
 
 class Resource(object):
 
-    def __init__(self, api):
+    sec_btw_tries = 5
+    """
+    How much time we need to wait for the next try to do the request to
+    the server if we got a HTTP_429_TOO_MANY_REQUESTS error. 
+    """
+
+    def __init__(self, api, tries=12):
         self.api = api
+        self.n_tries = tries
 
     def get_absolute_url(self, relative_url):
         return '{}{}{}'.format(
@@ -17,8 +31,23 @@ class Resource(object):
             relative_url)
 
     def action(self, keys, params=None, validate=True, overrides=None,
-               action=None, encoding=None, transform=None):
-        return self.api.client.action(
-            self.api.schema, keys, params=params, validate=validate,
-            overrides=overrides, action=action, encoding=encoding,
-            transform=transform)
+               action=None, encoding=None, transform=None,
+               n_tries=None, sec_btw_tries=None):
+        n_tries = self.n_tries if n_tries is None else n_tries
+        sec_btw_tries = self.sec_btw_tries \
+            if sec_btw_tries is None else sec_btw_tries
+        while n_tries:
+            try:
+                return self.api.client.action(
+                    self.api.schema, keys, params=params, validate=validate,
+                    overrides=overrides, action=action, encoding=encoding,
+                    transform=transform)
+            except exceptions.ErrorMessage as error:
+                if error.error["status"] == \
+                        status.HTTP_429_TOO_MANY_REQUESTS:
+                    _logger.info("Throttling the request! waiting {} seconds".
+                                 format(sec_btw_tries))
+                    n_tries -= 1
+                    time.sleep(sec_btw_tries)
+                else:
+                    raise error
